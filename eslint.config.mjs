6abc21df431e1745eb2b88982dc563f97b081e73
Michael Jakl <michael@jakl.cc>
2025-07-26 14:26:53 +0200

    Lint tests too

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,7 +7,7 @@ import tseslint from "typescript-eslint";
 export default tseslint.config(
   // ********** What not to lint **********
   {
-    ignores: ["*", "!src", "!src/**"],
+    ignores: ["*", "!src/**", "!tests/**"],
   },
 
   // ********** JavaScript settings **********
